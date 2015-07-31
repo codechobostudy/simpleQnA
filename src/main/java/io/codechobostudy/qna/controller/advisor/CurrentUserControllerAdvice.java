7dbb904094fe56328911293656c3ae1d20ea2dc7
SejongPark <devsejong@gmail.com>
2015-08-01 00:03:24 +0900

    refactor : 가독성 향상

--- src/main/java/io/codechobostudy/qna/controller/advisor/CurrentUserControllerAdvice.java
+++ src/main/java/io/codechobostudy/qna/controller/advisor/CurrentUserControllerAdvice.java
@@ -1,8 +1,6 @@
 package io.codechobostudy.qna.controller.advisor;
 
 import io.codechobostudy.qna.domain.auth.CurrentUser;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,11 +8,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 @ControllerAdvice
 public class CurrentUserControllerAdvice {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CurrentUserControllerAdvice.class);
-
     @ModelAttribute("currentUser")
     public CurrentUser getCurrentUser(Authentication authentication) {
-        return (authentication == null) ? null : (CurrentUser) authentication.getPrincipal();
+        if (authentication == null)
+            return null;
+        else
+            return (CurrentUser) authentication.getPrincipal();
     }
 
 
